72ab69c2143b33a0adcd711018dcdf4173a94175
t-harada <lightskeleton@gmail.com>
2018-09-07 18:23:27 +0900

    Androidプロジェクト：Activityがあるテンプレートで作成し、WebViewを追加した状態

---
+++ develop_rev2/app/src/main/java/com/example/ddd_rev2/myapplication_rev2/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.ddd_rev2.myapplication_rev2
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        webView.loadUrl("https://www.google.com/")
+    }
+}
